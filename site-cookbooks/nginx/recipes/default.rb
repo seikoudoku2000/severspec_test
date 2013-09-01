634de45b2f0f91683b1b9e49972fea8b88bd8a92
Yosuke Tomita <tomi-you@hotmail.co.jp>
2013-09-02 01:31:46 +0900

    add yum repo for nginx

--- site-cookbooks/nginx/recipes/default.rb
+++ site-cookbooks/nginx/recipes/default.rb
@@ -7,6 +7,12 @@
 # All rights reserved - Do Not Redistribute
 #
 
+if node["platform"] == "centos"
+	execute "add nginx yum repo" do
+		command "sudo rpm -ivh http://nginx.org/packages/centos/6/noarch/RPMS/nginx-release-centos-6-0.el6.ngx.noarch.rpm"
+	end
+end
+
 package "nginx" do
 	action :install
 end
